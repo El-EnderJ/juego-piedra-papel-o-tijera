304386acb11f679d4ab93d9ccc203b4469f8b8ff
El-EnderJ <130647064+El-EnderJ@users.noreply.github.com>
2023-04-24 09:53:26 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onGesture(Gesture.Shake, function () {
+    tool = randint(0, 2)
+    if (tool == 0) {
+        basic.showIcon(IconNames.SmallSquare)
+    } else if (tool == 1) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+    }
+})
+let tool = 0
+radio.setGroup(6)
+basic.forever(function () {
+	
+})
