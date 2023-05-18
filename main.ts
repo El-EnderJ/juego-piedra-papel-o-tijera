025a43e66eab3760fc24f42b43317df329bea10f
El-EnderJ <130647064+El-EnderJ@users.noreply.github.com>
2023-05-18 10:47:11 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,15 +1,69 @@
-input.onGesture(Gesture.Shake, function () {
-    tool = randint(0, 2)
-    if (tool == 0) {
-        basic.showIcon(IconNames.SmallSquare)
-    } else if (tool == 1) {
-        basic.showIcon(IconNames.Square)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        if (Lo_que_has_elegido == 1) {
+            basic.showString("Empate")
+        } else if (Lo_que_has_elegido == 2) {
+            basic.showString("Victoria")
+        } else {
+            basic.showString("Derrota")
+        }
+    } else if (receivedNumber == 2) {
+        if (Lo_que_has_elegido == 1) {
+            basic.showString("Victoria")
+        } else if (Lo_que_has_elegido == 2) {
+            basic.showString("Empate")
+        } else {
+            basic.showString("Derrota")
+        }
     } else {
-        basic.showIcon(IconNames.Scissors)
+        if (Lo_que_has_elegido == 1) {
+            basic.showString("Derrota")
+        } else if (Lo_que_has_elegido == 2) {
+            basic.showString("Victoria")
+        } else {
+            basic.showString("Empate")
+        }
     }
 })
-let tool = 0
-radio.setGroup(6)
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(1)
+    Lo_que_has_elegido = 1
+    basic.showLeds(`
+        . # # . #
+        # # # # #
+        . # # # #
+        . # # # .
+        # # # . .
+        `)
 })
+input.onButtonPressed(Button.AB, function () {
+    radio.sendNumber(3)
+    Lo_que_has_elegido = 3
+    basic.showLeds(`
+        # # . . #
+        # # . # .
+        . . # . .
+        # # . # .
+        # # . . #
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(2)
+    Lo_que_has_elegido = 2
+    basic.showLeds(`
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        # # # # #
+        `)
+})
+let Lo_que_has_elegido = 0
+radio.setGroup(56)
+images.createBigImage(`
+    # # # . # # . # # #
+    # . . . # # . # . .
+    # # # . # # . # # #
+    . . # . # # . . . .
+    # # # . # # . . . #
+    `).scrollImage(1, 200)
